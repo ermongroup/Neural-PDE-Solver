84940abb0ed34830f698988175f6b11377a2938a
Tim Hsieh <juntinghsieh@gmail.com>
2018-08-06 15:44:46 -0700

    Testing code for plotting.

--- testing/plot.py
+++ testing/plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -6,10 +7,21 @@ import matplotlib.pyplot as plt
 x = np.arange(10)
 a = x ** 2
 b = x ** 2.1
+fig = plt.figure()
 plt.plot(x, a, label='a')
 plt.plot(x, b, label='b')
 plt.xlabel('iterations')
 plt.legend()
 plt.title('Test')
-plt.savefig('test.png')
+#plt.savefig('test.png')
+
+# Save to numpy array
+fig.canvas.draw()
+w, h = fig.canvas.get_width_height()
+data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+data = data.reshape((h, w, -1))
+img = Image.fromarray(data)
+print(img.size)
+img.save('tmp.png')
+
 plt.close()
