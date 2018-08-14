4ebcee3e4e0168a15ebb329df653dc3e458c2f79
Tim Hsieh <juntinghsieh@gmail.com>
2018-08-13 23:22:54 -0700

    Model working! Change to fd residual. One eigenvalue = 1.

--- models/iterators/unet_iterator.py
+++ models/iterators/unet_iterator.py
@@ -12,23 +12,27 @@ class UNetIterator(Iterator):
     self.fd_update_kernel = torch.Tensor(utils.fd_update_kernel).view(1, 1, 3, 3).cuda()
     self.fd_loss_kernel = torch.Tensor(utils.fd_loss_kernel * (-0.25)).view(1, 1, 3, 3).cuda()
 
-    self.downsample = nn.AvgPool2d(2, stride=2)
-    self.layers = nn.Conv2d(1, 1, 3, stride=1, padding=0, bias=False)
+    self.conv1 = nn.Conv2d(1, nf, 3, stride=2, padding=0, bias=False)
+    self.conv2 = nn.Conv2d(nf, 1, 3, stride=1, padding=0, bias=False)
 
   def forward(self, x, bc):
     '''
     x: size (batch_size x 1 x image_size x image_size)
     return: same size
     '''
-    y = F.conv2d(x, self.fd_loss_kernel)
-    y = self.downsample(y)
-    # Replicate padding. Conv2d uses zero-pad, which doesn't make sense for us.
-    y = F.pad(y, (1, 1, 1, 1), 'replicate')
-    y = self.layers(y)
+    z = F.conv2d(x, self.fd_update_kernel)
+    y = x[:, :, 1:-1, 1:-1] - z
+
+    # replicate padding. Conv2d uses zero-pad, which doesn't make sense for us.
+    y = F.pad(y, (0, 1, 0, 1), mode='replicate')
+    y = self.conv1(y) # downsample by 2
+    y = F.pad(y, (1, 1, 1, 1), mode='replicate')
+    y = self.conv2(y)
     # Upsample: (image_size - 2, image_size - 2)
     y = F.interpolate(y, scale_factor=2, mode='bilinear', align_corners=False)
 
-    y = x[:, :, 1:-1, 1:-1] + y # residual
+    y = z + y # residual
+
     y = self.activation(y)
     # Set boundary
     y = utils.pad_boundary(y.squeeze(1), bc).unsqueeze(1) # same size as x
